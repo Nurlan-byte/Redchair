618602a3a1416120c3e355e26b23766d5c93dda7
Yussupov Nurlan <noooorlan2007@gmail.com>
2026-08-16 12:20:47 +0500

    feat: implement user authentication

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,9 +1,13 @@
 from datetime import UTC, datetime, timedelta
 
 import jwt
+import models
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from .core.config import settings
+from .core.database import get_db
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.api_v1_prefix}/login")
 
@@ -21,3 +25,26 @@ def create_access_token(data: dict):
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        user_id = int(payload["user_id"])
+    except jwt.PyJWTError, KeyError, TypeError, ValueError:
+        raise credentials_exception from None
+
+    if user_id is None:
+        raise credentials_exception
+
+    user = await db.get(models.User, user_id)
+
+    if user is None:
+        raise credentials_exception
+
+    return user
